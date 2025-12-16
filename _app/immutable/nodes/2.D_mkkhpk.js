54cae8ced08a9b4c54a19430cdc45c74ba910f01
yrno <henriksfurdal@outlook.com>
2025-12-16 08:15:55 +0100

    Updates

---
+++ _app/immutable/nodes/2.D_mkkhpk.js
@@ -0,0 +1,23 @@
+import{f as a,a as i}from"../chunks/CAwsjMv0.js";import"../chunks/CdtIOKgr.js";import{j as l,k as o,n as m}from"../chunks/C4h8WvNO.js";import{N as c,S as p}from"../chunks/Y-zw11NB.js";var d=a(`<div><h1>Welcome to my page</h1> <p>I haven't done anything cool with this yet, I'm just setting it up, but feel free to hit me up
+			on <a target="_blank" href="https://discord.com/users/286142874758938625">Discord</a> and tell me
+			how awesome this website is...</p></div> <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
+		sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+		nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+		sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p> <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
+		sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+		nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+		sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p> <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
+		sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+		nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+		sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p> <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
+		sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+		nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+		sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p> <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+		pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean
+		sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+		nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+		sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>`,1),v=a("<!> <!>",1);function I(t){var e=v(),s=l(e);c(s);var n=o(s,2);p(n,{children:(u,g)=>{var r=d();m(10),i(u,r)},$$slots:{default:!0}}),i(t,e)}export{I as component};
