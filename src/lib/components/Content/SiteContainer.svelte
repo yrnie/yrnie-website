155c1bd19bfc2662b2531654f2eefbb046c2dde8
yrno <henriksfurdal@outlook.com>
2025-12-17 21:35:28 +0100

    add error/404 page (not 100% on the implementation, we'll see)

--- src/lib/components/Content/SiteContainer.svelte
+++ src/lib/components/Content/SiteContainer.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import Footer from '../Navigation/Footer.svelte';
+	export let background_tags: String =
+		'bg-linear-to-r from-blue-500 from-30% via-blue-400 to-blue-500 to-70% bg-fixed bg-center';
 </script>
 
-<div
-	class="m-auto bg-linear-to-r from-blue-500 from-30% via-blue-400 to-blue-500 to-70% bg-fixed bg-center"
->
+<div class="m-auto {background_tags}">
 	<div class="sm:p-5 lg:p-10">
 		<div
 			class="mx-auto flex bg-neutral-800/60 sm:max-w-xl sm:rounded-2xl sm:backdrop-blur-sm md:max-w-2xl md:rounded-4xl md:p-4 lg:max-w-4xl lg:p-10 xl:max-w-6xl xl:p-12"
---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import SiteContainer from '$lib/components/Content/SiteContainer.svelte';
+	import robot from '$lib/assets/error_robot.png';
+</script>
+
+<SiteContainer background_tags="bg-gray-900">
+	<div class="items-center justify-center">
+		<h1 class="m-auto">Something went wrong...</h1>
+		<p class="m-auto">Error: {page.error?.message}</p>
+		<a href="/" class="m-auto">Go to homepage</a>
+		<img src={robot} alt="shrugging robot" class="m-auto lg:size-full" />
+	</div>
+</SiteContainer>
