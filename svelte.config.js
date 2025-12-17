bc952642e3b8ac654c4f2a5a527b24fb94248936
yrno <henriksfurdal@outlook.com>
2025-12-17 21:56:11 +0100

    change base url in svelte config for testing purposes

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/yrnie-website'
 		}
 	}
 };
